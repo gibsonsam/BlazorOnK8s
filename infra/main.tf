6164ec45f81d93237841555ba7105101af71ce0e
Sam Gibson <samgibsonmain@gmail.com>
2025-08-13 21:12:49 +0100

    Attach ACR "pull" policy to AKS

--- infra/main.tf
+++ infra/main.tf
@@ -56,3 +56,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
     Environment = var.environment
   }
 }
+
+# Attach ACR "Pull" policy to AKS
+resource "azurerm_role_assignment" "kubweb_to_acr" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
